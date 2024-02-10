a7e311b687e5e6ad59f945ece9cc7f285725292a
Glenn Nelson <glenn@hexcoder.us>
2024-02-10 07:26:51 -0500

    Fix Formatting

--- library/src/DefaultAuthenticator.ts
+++ library/src/DefaultAuthenticator.ts
@@ -19,43 +19,43 @@ export class DefaultAuthenticator {
         const cookieConfiguration: CookieConfiguration = DefaultAuthenticator.buildCookieConfiguration(configuration.cookieConfiguration);
 
         const cognitoTokenVerifierFactory: CognitoTokenVerifierFactory = new DefaultCognitoTokenVerifierFactory(
-        cognitoConfiguration,
+            cognitoConfiguration,
         );
 
         const defaultLogger: Logger = new DefaultLogger();
 
         this.authenticator = new Authenticator(
-        new DefaultCloudFrontResponseBuilder(),
-        new DefaultCognitoClient(
-            cognitoConfiguration,
+            new DefaultCloudFrontResponseBuilder(),
+            new DefaultCognitoClient(
+                cognitoConfiguration,
+                defaultLogger,
+            ),
+            new DefaultCognitoRequestBuilder(
+                cognitoConfiguration,
+            ),
+            new DefaultCookieRepository(
+                cookieConfiguration,
+            ),
             defaultLogger,
-        ),
-        new DefaultCognitoRequestBuilder(
-            cognitoConfiguration,
-        ),
-        new DefaultCookieRepository(
-            cookieConfiguration,
-        ),
-        defaultLogger,
-        cognitoTokenVerifierFactory.createAccessTokenVerifier(),
-        cognitoTokenVerifierFactory.createIdTokenVerifier(),
+            cognitoTokenVerifierFactory.createAccessTokenVerifier(),
+            cognitoTokenVerifierFactory.createIdTokenVerifier(),
         );
     }
 
     static buildCookieConfiguration(cookieConfiguration?: DefaultAuthenticatorCookieConfiguration): CookieConfiguration {
         return {
-        // TODO: Review Default!
-        disableDomain: (cookieConfiguration && typeof cookieConfiguration.disableDomain === 'boolean') ? cookieConfiguration.disableDomain : false,
-        // TODO: Review Default!
-        expirationDays: (cookieConfiguration && typeof cookieConfiguration.expirationDays === 'number') ? cookieConfiguration.expirationDays : 365,
-        // TODO: Review Default!
-        httpOnly: (cookieConfiguration && typeof cookieConfiguration.httpOnly === 'boolean') ? cookieConfiguration.httpOnly : undefined,
-        // We must intentionally set the `cookiePath` to `/`, providing access to all paths on the domain.
-        // Otherwise, the path appears set by the browser(?) and results in duplicate cookies and a redirect loop.
-        // See: https://github.com/awslabs/cognito-at-edge/issues/69
-        path: (cookieConfiguration && typeof cookieConfiguration.path === 'string') ? cookieConfiguration.path : '/',
-        // TODO: Review Default!
-        sameSite: (cookieConfiguration && cookieConfiguration.sameSite !== undefined && SAME_SITE_VALUES.includes(cookieConfiguration.sameSite)) ? cookieConfiguration.sameSite : undefined,
+            // TODO: Review Default!
+            disableDomain: (cookieConfiguration && typeof cookieConfiguration.disableDomain === 'boolean') ? cookieConfiguration.disableDomain : false,
+            // TODO: Review Default!
+            expirationDays: (cookieConfiguration && typeof cookieConfiguration.expirationDays === 'number') ? cookieConfiguration.expirationDays : 365,
+            // TODO: Review Default!
+            httpOnly: (cookieConfiguration && typeof cookieConfiguration.httpOnly === 'boolean') ? cookieConfiguration.httpOnly : undefined,
+            // We must intentionally set the `cookiePath` to `/`, providing access to all paths on the domain.
+            // Otherwise, the path appears set by the browser(?) and results in duplicate cookies and a redirect loop.
+            // See: https://github.com/awslabs/cognito-at-edge/issues/69
+            path: (cookieConfiguration && typeof cookieConfiguration.path === 'string') ? cookieConfiguration.path : '/',
+            // TODO: Review Default!
+            sameSite: (cookieConfiguration && cookieConfiguration.sameSite !== undefined && SAME_SITE_VALUES.includes(cookieConfiguration.sameSite)) ? cookieConfiguration.sameSite : undefined,
         };
     }
 
